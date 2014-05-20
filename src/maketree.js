ee00a90fe5ff41f8fbea7a5aa522981a525b289f
Will Long <will.long043@gmail.com>
2014-05-20 16:44:37 -0400

    Removed some comments.

--- src/maketree.js
+++ src/maketree.js
@@ -1,8 +1,4 @@
 function makeTreeFromTokens(tokens) {
-    //return { type: "Program", body: [left, right] };
-    // for (var i = 0, len = tokens.length; i < len; i++) {
-    //     console.log(tokens[i]);
-    // }
     return { type: "Program", body: makeStatement(tokens, 0) };
 }
 
